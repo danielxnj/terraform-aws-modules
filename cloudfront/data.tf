4d5118225a488c6dc580f7ace7217132750684bb
xneyder <xneyder@gmail.com>
2023-12-20 08:56:16 -0500

    Refactor AWS CloudFront cache policy in data.tf

--- cloudfront/data.tf
+++ cloudfront/data.tf
@@ -1,6 +1,6 @@
 data "aws_cloudfront_cache_policy" "default_cache_behavior" {
-  for_each = { for cache_behavior in var.default_cache_behavior : cache_behavior.target_origin_id => cache_behavior if lookup(cache_behavior, "cache_policy_name", null) != null }
-  name     = each.value.cache_policy_name
+  count = lookup(var.default_cache_behavior, "cache_policy_name", null) != null ? 1 : 0
+  name  = var.default_cache_behavior.cache_policy_name
 }
 
 data "aws_cloudfront_cache_policy" "ordered_cache_behavior" {
