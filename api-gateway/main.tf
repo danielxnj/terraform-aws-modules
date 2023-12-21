23aad41528ec2f8c641740c8251fd39204b7d822
xneyder <xneyder@gmail.com>
2023-12-21 13:02:19 -0500

    Refactor resource creation in main.tf

--- api-gateway/main.tf
+++ api-gateway/main.tf
@@ -121,10 +121,8 @@ locals {
 }
 
 resource "aws_api_gateway_resource" "this" {
-  for_each = local.enabled ? local.api_resources : {}
+  for_each    = local.enabled ? local.api_resources : {}
   rest_api_id = aws_api_gateway_rest_api.this[0].id
   path_part   = each.value.path_part
-  parent_id   = each.key == "/" ? local.root_resource_id : 
-                (each.value.parent_path == "/" ? local.root_resource_id : 
-                aws_api_gateway_resource.this[each.value.parent_path].id)
+  parent_id   = each.key == "/" ? local.root_resource_id : (each.value.parent_path == "/" ? local.root_resource_id : aws_api_gateway_resource.this[each.value.parent_path].id)
 }
