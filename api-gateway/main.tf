261950765757cc4c7869ccccd2212b5ef595b7d6
xneyder <xneyder@gmail.com>
2023-12-21 18:32:20 -0500

    Refactor API Gateway integration configuration

--- api-gateway/main.tf
+++ api-gateway/main.tf
@@ -505,26 +505,26 @@ resource "aws_api_gateway_integration" "depth_0" {
 
 
 resource "aws_api_gateway_integration" "depth_3" {
-  for_each = local.enabled ? { for path, info in local.all_methods : path => info.method.integration if info.depth == 3 && try(info.method.integration, null) != null } : {}
+  for_each = local.enabled ? { for path, info in local.all_methods : path => info.method if info.depth == 3 && lookup("integration", info.method.integration, null) != null } : {}
 
   rest_api_id             = aws_api_gateway_rest_api.this[0].id
   resource_id             = aws_api_gateway_resource.depth_3[each.value.path].id
   http_method             = try(each.value.method, null)
-  integration_http_method = try(each.value.integration_http_method, null)
-  type                    = try(each.value.type, null)
-  connection_type         = try(each.value.connection_type, null)
-  connection_id           = try(each.value.connection_id, null)
-  uri                     = try(each.value.uri, null)
-  credentials             = try(each.value.credentials, null)
-  request_templates       = try(each.value.request_templates, null)
-  request_parameters      = try(each.value.request_parameters, null)
-  passthrough_behavior    = try(each.value.passthrough_behavior, null)
-  cache_key_parameters    = try(each.value.cache_key_parameters, null)
-  cache_namespace         = try(each.value.cache_namespace, null)
-  content_handling        = try(each.value.content_handling, null)
-  timeout_milliseconds    = try(each.value.timeout_milliseconds, null)
+  integration_http_method = try(each.value.integration.integration_http_method, null)
+  type                    = try(each.value.integration.type, null)
+  connection_type         = try(each.value.integration.connection_type, null)
+  connection_id           = try(each.value.integration.connection_id, null)
+  uri                     = try(each.value.integration.uri, null)
+  credentials             = try(each.value.integration.credentials, null)
+  request_templates       = try(each.value.integration.request_templates, null)
+  request_parameters      = try(each.value.integration.request_parameters, null)
+  passthrough_behavior    = try(each.value.integration.passthrough_behavior, null)
+  cache_key_parameters    = try(each.value.integration.cache_key_parameters, null)
+  cache_namespace         = try(each.value.integration.cache_namespace, null)
+  content_handling        = try(each.value.integration.content_handling, null)
+  timeout_milliseconds    = try(each.value.integration.timeout_milliseconds, null)
   dynamic "tls_config" {
-    for_each = try(each.value.tls_config, null)
+    for_each = try(each.value.integration.tls_config, null)
 
     content {
       insecure_skip_verification = tls_config.value.insecure_skip_verification
