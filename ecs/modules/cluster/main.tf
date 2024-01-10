f022fdffafac2983f5452d314682fd823718d50b
xneyder <xneyder@gmail.com>
2024-01-09 22:10:35 -0500

    Commented out unused code in main.tf

--- ecs/modules/cluster/main.tf
+++ ecs/modules/cluster/main.tf
@@ -18,35 +18,35 @@ resource "aws_ecs_cluster" "this" {
 
   name = var.cluster_name
 
+  # dynamic "configuration" {
+  #   for_each = [var.cluster_configuration]
+
+  #   content {
+  #     dynamic "execute_command_configuration" {
+  #       for_each = try(merge(local.execute_command_configuration, configuration.value.execute_command_configuration), [{}])
+
+  #       content {
+  #         kms_key_id = try(execute_command_configuration.value.kms_key_id, null)
+  #         logging    = try(execute_command_configuration.value.logging, "DEFAULT")
+
+  #         dynamic "log_configuration" {
+  #           for_each = try(execute_command_configuration.value.log_configuration, [])
+
+  #           content {
+  #             cloud_watch_encryption_enabled = try(log_configuration.value.cloud_watch_encryption_enabled, null)
+  #             cloud_watch_log_group_name     = try(log_configuration.value.cloud_watch_log_group_name, null)
+  #             s3_bucket_name                 = try(log_configuration.value.s3_bucket_name, null)
+  #             s3_bucket_encryption_enabled   = try(log_configuration.value.s3_bucket_encryption_enabled, null)
+  #             s3_key_prefix                  = try(log_configuration.value.s3_key_prefix, null)
+  #           }
+  #         }
+  #       }
+  #     }
+  #   }
+  # }
+
   dynamic "configuration" {
     for_each = [var.cluster_configuration]
-
-    content {
-      dynamic "execute_command_configuration" {
-        for_each = try(merge(local.execute_command_configuration, configuration.value.execute_command_configuration), [{}])
-
-        content {
-          kms_key_id = try(execute_command_configuration.value.kms_key_id, null)
-          logging    = try(execute_command_configuration.value.logging, "DEFAULT")
-
-          dynamic "log_configuration" {
-            for_each = try(execute_command_configuration.value.log_configuration, [])
-
-            content {
-              cloud_watch_encryption_enabled = try(log_configuration.value.cloud_watch_encryption_enabled, null)
-              cloud_watch_log_group_name     = try(log_configuration.value.cloud_watch_log_group_name, null)
-              s3_bucket_name                 = try(log_configuration.value.s3_bucket_name, null)
-              s3_bucket_encryption_enabled   = try(log_configuration.value.s3_bucket_encryption_enabled, null)
-              s3_key_prefix                  = try(log_configuration.value.s3_key_prefix, null)
-            }
-          }
-        }
-      }
-    }
-  }
-
-  dynamic "configuration" {
-    for_each = !var.create_cloudwatch_log_group && length(var.cluster_configuration) > 0 ? [var.cluster_configuration] : []
 
     content {
       dynamic "execute_command_configuration" {
