81c4608f0248877bb54f3d01ba35304d8d2ccca5
xneyder <xneyder@gmail.com>
2024-01-10 22:58:35 -0500

    Update target_group_arn in aws_ecs_service resource

--- ecs/modules/service/main.tf
+++ ecs/modules/service/main.tf
@@ -90,7 +90,7 @@ resource "aws_ecs_service" "this" {
       container_name   = load_balancer.value.container_name
       container_port   = load_balancer.value.container_port
       elb_name         = try(load_balancer.value.elb_name, null)
-      target_group_arn = var.target_group_arn
+      target_group_arn = load_balancer.value.target_group_arn
     }
   }
 
