96c82b9560561b1a346ea2e63f38d8471385f62a
xneyder <xneyder@gmail.com>
2024-01-12 11:38:16 -0500

    Commented out security group outputs

--- ecs/modules/service/outputs.tf
+++ ecs/modules/service/outputs.tf
@@ -132,12 +132,12 @@ output "autoscaling_scheduled_actions" {
 # Security Group
 ################################################################################
 
-output "security_group_arn" {
-  description = "Amazon Resource Name (ARN) of the security group"
-  value       = try(aws_security_group.this[0].arn, null)
-}
+# output "security_group_arn" {
+#   description = "Amazon Resource Name (ARN) of the security group"
+#   value       = try(aws_security_group.this[0].arn, null)
+# }
 
-output "security_group_id" {
-  description = "ID of the security group"
-  value       = try(aws_security_group.this[0].id, null)
-}
+# output "security_group_id" {
+#   description = "ID of the security group"
+#   value       = try(aws_security_group.this[0].id, null)
+# }
