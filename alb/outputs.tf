92f4a398a50400a04bfbc302491683835b012ba5
xneyder <xneyder@gmail.com>
2024-01-11 23:14:23 -0500

    Add output for ALB listeners

--- alb/outputs.tf
+++ alb/outputs.tf
@@ -23,6 +23,11 @@ output "alb_zone_id" {
   value       = one(aws_lb.default[*].zone_id)
 }
 
+output "listeners" {
+  description = "The list of listener blocks of the ALB"
+  value       = aws_lb_listener.this
+}
+
 # output "security_group_id" {
 #   description = "The security group ID of the ALB"
 #   value       = one(aws_security_group.default[*].id)
