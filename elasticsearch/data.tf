237bc53d99a908cb56c0da6025aed00c594ebd0a
xneyder <xneyder@gmail.com>
2024-01-14 21:43:18 -0500

    Add default VPC data source
    This commit adds a new data source for the default VPC, allowing retrieval of its ID based on the provided VPC name.

--- elasticsearch/data.tf
+++ elasticsearch/data.tf
@@ -2,6 +2,13 @@ locals {
   is_valid_network_config = length(var.vpc_options) > 0 && alltrue([for config in var.vpc_options : can(config.subnet_names)])
 }
 
+data "aws_vpc" "default" {
+  count = 1
+  tags = {
+    Name = var.vpc_name
+  }
+}
+
 data "aws_subnet" "default" {
   count = local.is_valid_network_config ? length(var.vpc_options[0].subnet_names) : 0
   vpc_id = var.vpc_name != null ? data.aws_vpc.default[0].id : var.vpc_id
