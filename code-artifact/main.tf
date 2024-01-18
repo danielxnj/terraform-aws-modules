d69abc2dfcb19fdc5f4bd973ff788e84d017216c
xneyder <xneyder@gmail.com>
2024-01-17 21:46:35 -0500

    Add code-artifact repository description and external connections

--- code-artifact/main.tf
+++ code-artifact/main.tf
@@ -9,12 +9,36 @@ resource "aws_codeartifact_domain_permissions_policy" "this" {
   count = var.enabled && var.domain_policy_document != null ? 1 : 0
   domain = aws_codeartifact_domain[0].this.domain
   policy_document = var.domain_policy_document
+  domain_owner = var.domain_policy_domain_owner
+  policy_revision = var.domain_policy_revision
+
 }
 
 resource "aws_codeartifact_repository" "this" {
   count = var.enabled ? 1 : 0
   repository = var.repository
   domain     = aws_codeartifact_domain[0].this.domain
+  description = var.description
+  domain_owner = var.repository_domain_owner
+
+  dynamic "external_connections" {
+    for_each = var.external_connections
+    content {
+      external_connection_name = external_connections.value.external_connection_name
+      package_format = external_connections.value.package_format
+      status = external_connections.value.status
+    }
+    }
+
+    dynamic "upstreams" {
+    for_each = var.upstreams
+    content {
+      repository_name = upstreams.value.repository_name
+      external_connection_name = upstreams.value.external_connection_name
+    }
+    }
+
+    tags = var.repository_tags
 }
 
 resource "aws_codeartifact_repository_permissions_policy" "this" {
@@ -22,4 +46,6 @@ resource "aws_codeartifact_repository_permissions_policy" "this" {
   domain = aws_codeartifact_domain[0].this.domain
   repository = aws_codeartifact_repository[0].this.repository
   policy_document = var.repository_policy_document
+  domain_owner = var.repository_policy_domain_owner
+  policy_revision = var.repository_policy_revision
 }
--- code-artifact/variables.tf
+++ code-artifact/variables.tf
@@ -37,3 +37,57 @@ variable "repository_policy_document" {
     type = string
     default = null
 }
+
+variable "domain_policy_domain_owner" {
+    description = "The account number of the domain owner."
+    type = string
+    default = null
+}
+
+variable "domain_policy_revision" {
+    description = "The current revision of the policy."
+    type = string
+    default = null
+}
+
+variable "description" {
+  description = "The description of the repository."
+  type = string
+  default = null
+}
+
+variable "repository_domain_owner" {
+    description = "The account number of the repository owner."
+    type = string
+    default = null
+}
+
+variable "external_connections" {
+    description = "A list of external connections for the repository."
+    type = list(string)
+    default = []
+}
+
+variable "upstreams" {
+    description = "A list of upstream repositories to associate with the repository."
+    type = list(string)
+    default = []
+}
+
+variable "repository_tags" {
+    description = "A map of tags to assign to the resource."
+    type = map(string)
+    default = {}
+}
+
+variable "repository_policy_domain_owner" {
+    description = "The account number of the repository owner."
+    type = string
+    default = null
+}
+
+variable "repository_policy_revision" {
+    description = "The current revision of the policy."
+    type = string
+    default = null
+}
