9289e36fc578c157205d8c93be8e87da23a01a56
xneyder <xneyder@gmail.com>
2024-01-18 18:29:45 -0500

    Fix domain and repository references in main.tf

--- code-artifact/main.tf
+++ code-artifact/main.tf
@@ -17,7 +17,7 @@ resource "aws_codeartifact_domain_permissions_policy" "this" {
 resource "aws_codeartifact_repository" "this" {
   count = var.enabled ? 1 : 0
   repository = var.repository
-  domain     = aws_codeartifact_domain[0].this.domain
+  domain     = aws_codeartifact_domain.this[0].domain
   description = var.description
   domain_owner = var.repository_domain_owner
 
@@ -43,8 +43,8 @@ resource "aws_codeartifact_repository" "this" {
 
 resource "aws_codeartifact_repository_permissions_policy" "this" {
   count = var.enabled && var.repository_policy_document != null ? 1 : 0
-  domain = aws_codeartifact_domain[0].this.domain
-  repository = aws_codeartifact_repository[0].this.repository
+  domain = aws_codeartifact_domain.this[0].domain
+  repository = aws_codeartifact_repository.this[0].repository
   policy_document = var.repository_policy_document
   domain_owner = var.repository_policy_domain_owner
   policy_revision = var.repository_policy_revision
